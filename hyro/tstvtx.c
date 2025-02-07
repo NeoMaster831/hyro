18943db21a31c0ccc0bf468f7cebd069a563f129
wane <wane@VM>
2025-02-07 18:58:47 +0900

    oops i forgot to delete `tstvtx.c`

--- hyro/tstvtx.c
+++
@@ -1,7 +0,0 @@
-#include "tstvtx.h"
-
-void TstHyroTestcall(UINT64 p1, UINT64 p2, UINT64 p3) {
-  ATstHyroVmcall(0, p1, p2, p3);
-}
-
-UINT64 TstHyroXorcall(UINT64 p1, UINT64 p2) { return ATstHyroCpuid(0, p1, p2); }
